a7f73062a2558d56880f087bc4aee3f865a89eeb
arzafran <franco@darkroom.engineering>
2024-10-09 14:59:31 -0300

    updates and fixes

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Apply Lenis to any website through this chrome extension.
 
 ### ToDo:
 
-- [ ] Inject the script from an external source (jsdlvr?)
-- [ ] Enable/disable styles on the icon
-- [ ] Fix disable (instead of refreshing the page)
-- [ ] Only toggle in excecute.js instead of injecting it all
+- [x] Inject the script from an external source (jsdlvr?)
+- [x] Enable/disable styles on the icon
+- [x] Fix disable (instead of refreshing the page)
+- [x] Only toggle in excecute.js instead of injecting it all
--- background.js
+++ background.js
@@ -1,15 +1,32 @@
-// event to run execute.js content when extension's button is clicked
-chrome.action.onClicked.addListener(execScript);
+// Event to toggle Lenis when extension's button is clicked
+chrome.action.onClicked.addListener(async (tab) => {
+  chrome.tabs.sendMessage(tab.id, { action: "toggleLenis" });
+});
 
-async function execScript() {
-  const tabId = await getTabId();
-  chrome.scripting.executeScript({
-    target: { tabId: tabId },
-    files: ["execute.js"],
+// Function to update the icon
+function updateIcon(enabled) {
+  const path = enabled ? {
+    16: 'icon16.png',
+    48: 'icon48.png',
+    128: 'icon128.png'
+  } : {
+    16: 'icon16-gray.png',
+    48: 'icon48-gray.png',
+    128: 'icon128-gray.png'
+  };
+  chrome.action.setIcon({ path: path });
+}
+
+// Listen for changes in storage
+chrome.storage.onChanged.addListener((changes, namespace) => {
+  if (namespace === 'local' && 'smooth' in changes) {
+    updateIcon(changes.smooth.newValue);
+  }
+});
+
+// Set initial icon state on extension load
+chrome.runtime.onStartup.addListener(() => {
+  chrome.storage.local.get(['smooth'], (result) => {
+    updateIcon(result.smooth || false);
   });
-}
-
-async function getTabId() {
-  const tabs = await chrome.tabs.query({ active: true, currentWindow: true });
-  return tabs.length > 0 ? tabs[0].id : null;
-}
+});
